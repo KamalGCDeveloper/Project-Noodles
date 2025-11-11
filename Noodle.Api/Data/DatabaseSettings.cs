55d628045baf5a6aab8e1e059910fbb972f2063a
Watkins <tmh.pug@gmail.com>
2025-11-11 16:16:57 +0700

    Remove Noodle.Api folder from repository

--- Noodle.Api/Data/DatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Noodle.Api.Data
-{
-    public class DatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-    }
-}
